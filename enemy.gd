b126a0b7f554da8cbf393ce750756d0371844a4d
Helen Yang <helen.xu.yang@gmail.com>
2026-07-25 11:57:02 -0700

    fix health

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,7 @@
 class_name Enemy
 extends Area2D
 
-var max_health = 50.0
+var max_health: float = 50.0
 var health = max_health
 var follow_rad = 200
 var follow_speed = 1
@@ -17,7 +17,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if((position - player.position).length() > follow_rad):
 		position += (player.position - position).normalized()*follow_speed
-	
 	health_bar.value = health/max_health
 	if self.health <= 0:
 		self.queue_free()
@@ -27,7 +26,8 @@ static func create_fast_enemy() -> Enemy:
 	var instance: Enemy = scene.instantiate()
 	instance.scale = Vector2(0.5, 0.5)
 	instance.follow_speed = 3
-	instance.max_health = 10
+	instance.max_health = 10.0
+	instance.health = instance.max_health
 	return instance
 
 static func create_slow_enemy() -> Enemy:
@@ -35,7 +35,8 @@ static func create_slow_enemy() -> Enemy:
 	var instance: Enemy = scene.instantiate()
 	instance.scale = Vector2(1.5, 1.5)
 	instance.follow_speed = 0.5
-	instance.max_health = 100
+	instance.max_health = 100.0
+	instance.health = instance.max_health
 	return instance
 
 static var enemy_creators: Array[Callable] = [
